d02f256f35658f5336a5818080aaa8f9d348d963
colerusson <colerusson2@gmail.com>
2023-01-30 15:08:35 -0700

    wrote the code to calculate the mean and variance of an array

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import argparse as ap
 def main(array):
     # Write to compute the variance and the mean of a given list of numbers
     # Make sure that your terminal output matches the terminal output of the example given on the instructions.
-
-    return None
+    sumCounter = 0
+    size = len(array)
+    for i in array:
+        sumCounter += i
+    mean = sumCounter / size
+    return mean
 
 
 if __name__ == "__main__":
@@ -13,4 +17,11 @@ if __name__ == "__main__":
     argParse.add_argument("--array", nargs="+", type=int,
                           help="Input a list of numbers to compute the variance and mean of")
     parsedArgs = argParse.parse_args()
-    main(parsedArgs.array)
+    output = main(parsedArgs.array)
+    squareCounter = 0
+    for i in parsedArgs.array:
+        squareCounter += (output - i)**2
+    variance = squareCounter / len(parsedArgs.array)
+
+    print('mean = ' + str(output))
+    print('variance = ' + str(variance))
